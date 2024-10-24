97111da7a566703027f06358bb61a2e230839929
mmaglio <maximiliano.maglio@ing.austral.edu.ar>
2024-10-24 15:47:22 -0300

    fix(mmaxi): fix rest-connection issue

--- src/main/kotlin/language/controller/LanguageController.kt
+++ src/main/kotlin/language/controller/LanguageController.kt
@@ -8,11 +8,9 @@ import language.model.dto.ValidationResponse
 import language.service.LanguageService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["*"]) //
 @RestController
 @RequestMapping("/language")
 class LanguageController(
